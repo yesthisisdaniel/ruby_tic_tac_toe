91100da917fff11bf02b9d7303cbc0c1fb949f18
Daniel Nowak <danieln311@gmail.com>
2026-09-17 10:40:17 +0000

    Create method that prints centered grid for tic tac toe game

--- lib/grid.rb
+++ lib/grid.rb
@@ -6,7 +6,8 @@ class Grid
     end
 
     def print_grid()
-        new_grid = @grid.each_slice(3).to_a.map { |row| row.join( " | " ) }
+        new_grid = @grid.each_slice(3).to_a.map { |row| row.map { |cell| cell.to_s.center(3) }.join(" | ") }
+        new_grid = new_grid.join("\n---------------\n")
         puts new_grid
     end
 

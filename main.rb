fbe526e53f6fbd7d0bd06bc2f87cea2b49c80485
Daniel Nowak <danieln311@gmail.com>
2026-09-18 10:45:13 +0000

    Correct script for get_player_type method and create_player method

--- main.rb
+++ main.rb
@@ -2,10 +2,23 @@ require_relative "lib/grid"
 require_relative "lib/game"
 require_relative "lib/player"
 
-player1 = ""
-player2 = ""
+def create_player()
+    puts "What is your name?"
+    name = gets.chomp
+    return Player.new("human", name)
+end
 
 def get_player_types()
-    player1 = gets.chomp
+    create_player()
+    puts "Will player 2 be a computer?"
+    input = gets.chomp
+    
+    if input == "yes"
+        puts "Player 2 will be the computer."
+        player2 = Player.new("computer", "computer")
+    else
+        create_player()
+    end
+    return 
 end
 get_player_types()

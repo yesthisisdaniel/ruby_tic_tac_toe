c71d4c6fa71898666634a6ceddf9acf966d1804b
Daniel Nowak <danieln311@gmail.com>
2026-09-19 08:39:28 +0000

    Start to write play_game method

--- lib/game.rb
+++ lib/game.rb
@@ -1,9 +1,10 @@
 require_relative "grid"
 
 class Game
-    attr_accessor :new_grid
     def initialize()
-        @new_grid = Grid.new
-        @new_grid.print_grid
+        
     end
+
+    def play_game()
+
 end

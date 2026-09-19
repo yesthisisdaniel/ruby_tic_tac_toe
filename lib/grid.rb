bba1f890bf11bfda5cdcfcb36efda75a061b1be9
Daniel Nowak <danieln311@gmail.com>
2026-09-19 07:18:19 +0000

    Add colorize to game grid

--- lib/grid.rb
+++ lib/grid.rb
@@ -5,8 +5,8 @@ class Grid
     end
 
     def print_grid()
-        new_grid = @grid.each_slice(3).to_a.map { |row| row.map { |cell| cell.to_s.center(3) }.join(" | ") }
-        new_grid = new_grid.join("\n---------------\n")
+        new_grid = @grid.each_slice(3).to_a.map { |row| row.map { |cell| cell.to_s.center(3) }.join(" | ").magenta }
+        new_grid = new_grid.join("\n---------------\n").magenta
         puts new_grid
     end
 

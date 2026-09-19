48fff6f3c09ac02be485ebcae67adffd40eced92
Daniel Nowak <danieln311@gmail.com>
2026-09-19 08:32:07 +0000

    Create update_grid method in grid.rb

--- lib/grid.rb
+++ lib/grid.rb
@@ -1,13 +1,24 @@
+require "colorize"
+
 class Grid 
-    attr_accessor :grid
+    attr_accessor :grid, :new_grid
     def initialize()
         @grid = [nil, nil, nil, nil, nil, nil, nil, nil, nil]
     end
 
     def print_grid()
-        new_grid = @grid.each_slice(3).to_a.map { |row| row.map { |cell| cell.to_s.center(3) }.join(" | ").magenta }
-        new_grid = new_grid.join("\n---------------\n").magenta
+        @new_grid = @grid.each_slice(3).to_a.map { |row| row.map { |cell| cell.to_s.center(3) }.join(" | ").magenta }
+        @new_grid = new_grid.join("\n---------------\n").magenta
         puts new_grid
+    end
+
+    def update_grid(index, mark)
+        if @grid[index] != nil
+            return false
+        else
+            @grid[index] = mark
+            return true
+        end
     end
 
 end

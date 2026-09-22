6982dfc848626c72bfeaed0f23a61fedcc7ecbaa
Daniel Nowak <danieln311@gmail.com>
2026-09-22 13:53:25 +0000

    Add win_con array

--- lib/grid.rb
+++ lib/grid.rb
@@ -1,10 +1,12 @@
 require "colorize"
 
 class Grid 
-    attr_accessor :grid, :new_grid
+    attr_accessor :grid, :new_grid, :win_cons
     def initialize()
         @grid = [nil, nil, nil, nil, nil, nil, nil, nil, nil]
     end
+
+    @win_cons =  [[1, 2, 3], [1, 5, 9], [1, 4, 7], [2, 5, 8], [3, 6, 9], [4, 5, 6], [7, 8, 9], [3, 5, 7]]
 
     def print_grid()
         @new_grid = @grid.each_slice(3).to_a.map { |row| row.map { |cell| cell.to_s.center(3) }.join(" | ").magenta }
